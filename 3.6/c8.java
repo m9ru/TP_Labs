a40a9c6bfd0f6466595ac25659011147e4938c48
m9ru <max99ru@mail.ru>
2020-10-24 01:02:44 +0300

    33

--- 3.6/c8.java
+++ 3.6/c8.java
@@ -12,6 +12,10 @@ public class c8 {
                     cur = 0;
                 }
             }
+            if (cur>max && i == str.length()-1) {
+                max = cur;
+                cur = 0;
+            }
         }
         String ans = "";
         for (int i = 0; i<max; i++) {
@@ -20,6 +24,6 @@ public class c8 {
         return ans;
     }
     public static void main(String[] args) {
-        System.out.println(longestZero("0110001011000"));
+        System.out.println(longestZero("01100010110000"));
     }
 }
